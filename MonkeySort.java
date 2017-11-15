1be3ae0e7283b6d552e3240ca2c7b780cb869564
MonsterMannen <viktorsvensson94@gmail.com>
2017-11-15 18:58:43 +0100

    arch

---
+++ MonkeySort.java
@@ -0,0 +1,48 @@
+public class MonkeySort {
+    public int[] sort(int[] array){
+        while(!isSorted(array)){
+            array = shuffle(array);
+        }
+        return array;
+    }
+
+    private boolean isSorted(int[] array){
+        for(int i = 0; i < array.length - 1; i++){
+            if(array[i] > array[i+1]) return false;
+        }
+        return true;
+    }
+
+    private int[] shuffle(int[] array){
+        int rand = 0;
+        for(int i = 0; i < array.length; i++){
+            rand = (int)(Math.random() * array.length);
+            
+            // swap
+            int temp = array[i];
+            array[i] = array[rand];
+            array[rand] = temp; 
+        }
+        return array;
+    }
+
+    public int[] stringToIntArray(String[] array){
+        int[] intArray = new int[array.length];
+        for(int i = 0; i < array.length; i++){
+            intArray[i] = Integer.parseInt(array[i]);
+        }
+        return intArray;
+    }
+
+    public void printArray(int[] array){
+        for(int i = 0; i < array.length; i++)
+        System.out.print(array[i] + " ");
+        System.out.println();
+    }
+
+    // main
+    public static void main(String[] args){
+        MonkeySort ms = new MonkeySort();
+        ms.printArray(ms.sort(ms.stringToIntArray(args)));
+    }
+}
---
+++ SleepSort.java
@@ -0,0 +1,43 @@
+public class SleepSort {
+
+    public void sort(int[] array){
+        for(int i = 0; i < array.length; i++){
+            // new process
+            Thread ps = new SleepProcess(array[i]);
+            ps.start();
+        }
+    }
+
+    public int[] stringToIntArray(String[] array){
+        int[] intArray = new int[array.length];
+        for(int i = 0; i < array.length; i++){
+            intArray[i] = Integer.parseInt(array[i]);
+        }
+        return intArray;
+    }
+
+    // main
+    public static void main(String[] args){
+        SleepSort ss = new SleepSort();
+        ss.sort(ss.stringToIntArray(args));
+    }
+
+
+    private class SleepProcess extends Thread {
+        int value;
+
+        public SleepProcess(int v){
+            value = v;
+        }
+
+        public void run(){
+            try{
+                Thread.sleep(value * 100);
+                System.out.print(value + " ");
+            }catch(Exception ex){
+                ex.printStackTrace();
+            }
+        }
+    }
+
+}
